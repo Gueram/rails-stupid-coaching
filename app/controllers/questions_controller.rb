fac1323f18d35843214bc0d480899d8dc21396df
Gueram Minassian <gueram.minassian@gmail.com>
2020-11-09 15:37:43 +0100

    Exo2_ssCSS&optionnel

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  
+  def ask
+  end
+  
+  def answer
+    user_answer = params[:answer]   # renvoie au name:"answer" du form
+
+    if user_answer.downcase == "i am going to work!"
+      @coach_answer = "Great!"
+    elsif user_answer.end_with?("?")
+      @coach_answer = "Silly question, get dressed and go to work!"
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
